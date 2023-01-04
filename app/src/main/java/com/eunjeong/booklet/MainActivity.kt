88be48bd6d41c986fb473ca0fbeb7e845ac56a11
Eundding <dkan9634@naver.com>
2023-01-04 11:36:46 +0900

    feat : 처음 화면 구현

---
+++ app/src/main/java/com/eunjeong/booklet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eunjeong.booklet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

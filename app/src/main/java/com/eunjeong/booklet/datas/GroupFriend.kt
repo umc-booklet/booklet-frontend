14cb449b462d9fca009d5e4f9ab4a1c513bfd10e
Eundding <dkan9634@naver.com>
2023-01-14 22:36:31 +0900

    Feat : 그룹추가 구현

---
+++ app/src/main/java/com/eunjeong/booklet/datas/GroupFriend.kt
@@ -0,0 +1,15 @@
+package com.eunjeong.booklet.datas
+
+//class GroupFriend (
+//    var checked: Boolean,
+//    var img: Int,
+//    var name: String,
+//    var id: String
+//){}
+
+data class GroupFriend (
+    var checked: Boolean,
+    var img: Int,
+    var name: String,
+    var id: String
+)

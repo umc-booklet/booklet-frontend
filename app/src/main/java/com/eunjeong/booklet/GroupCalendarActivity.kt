e0ccc2b7889e44e816dd7ed3e6456c61ce5b966a
jiwoo <bum3201@naver.com>
2023-02-13 00:57:41 +0900

    날짜 연결

--- app/src/main/java/com/eunjeong/booklet/GroupCalendarActivity.kt
+++ app/src/main/java/com/eunjeong/booklet/GroupCalendarActivity.kt
@@ -16,6 +16,8 @@ import androidx.core.view.isVisible
 import com.eunjeong.booklet.databinding.ActivityCalendarBinding
 import com.eunjeong.booklet.databinding.ActivityGroupCalendarBinding
 import com.eunjeong.booklet.databinding.CalendarDayLayoutBinding
+import com.eunjeong.booklet.detailSchedule.DayScheduleFragment
+import com.eunjeong.booklet.detailSchedule.Detail
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.navigation.NavigationView
 import com.kizitonwose.calendar.core.*
@@ -953,19 +955,19 @@ class GroupCalendarActivity : AppCompatActivity(), NavigationView.OnNavigationIt
 
     // 달력에서 날짜 누르면 DayScheduleFragment가 나오고,
     // DayScheduleFragment에서 버튼을 누르면 TimeTableFragment로 가기 위한 함수
-    // interface :  DayScheduleFragment -> timetableFragment
-    override fun GoToTimeTableFragment(clickedDate: String) {
+    // interface : timetableFragment -> DayScheduleFragment
+    override fun GoToTimeTableFragment(clickedDate: String, plan: ArrayList<Detail>) {
         val bundle = Bundle()
         bundle.putString("Date", clickedDate)
+        bundle.putParcelableArrayList("Plan", plan)
 
         val transaction = this.supportFragmentManager.beginTransaction()
-        val timetablefragment = TimeTableFragment()
-        timetablefragment.arguments = bundle
+        val timeTableFragment = TimeTableFragment()
+        timeTableFragment.arguments = bundle
 
-        transaction.replace(binding.frameFragment.id, timetablefragment)
-        transaction.addToBackStack("timetableFragment")
+        transaction.replace(binding.frameFragment.id, timeTableFragment)
+        transaction.addToBackStack("timeTableFragment")
         transaction.commit()
-
     }
 
     // interface : timetableFragment -> DayScheduleFragment
@@ -978,7 +980,7 @@ class GroupCalendarActivity : AppCompatActivity(), NavigationView.OnNavigationIt
         dayschedulefragment.arguments = bundle
 
         transaction.replace(binding.frameFragment.id, dayschedulefragment)
-        transaction.addToBackStack("dayscheduleFragment").commitAllowingStateLoss()
+        transaction.addToBackStack("dayscheduleFragment")
         transaction.commit()
     }
 }
